807956fcaf9afb656fec350efe057163bfb352da
Khushi366 <khushiidesai2@gmail.com>
2024-08-28 22:18:51 +0530

    28-08-2024

--- self_practice/lib/ProductPage/Controller.dart
+++ self_practice/lib/ProductPage/Controller.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
 import 'package:http/http.dart' as https;
 import 'package:get/get.dart';
 import 'package:self_practice/Model/Product.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ProductDetailsController extends GetxController {
   var pid = ''.obs; // Observable for product ID
@@ -15,16 +16,35 @@ class ProductDetailsController extends GetxController {
     log('Received pid: ' + pid.value);
     fetchProducts();
   }
+  //Use Api
+  // fetchProducts() async {
+  //   Uri url = Uri.parse("https://fakestoreapi.com/products/${pid.value}");
+  //   var response = await https.get(url);
+  //   if (response.statusCode == 200) {
+  //     var body = response.body.toString();
+  //     var json = jsonDecode(body);
+  //
+  //     log(body.toString());
+  //     product.value = Product.fromJson(json);
+  //   }
+  // }
+
+
+  // Use SharedPreference (Api Get data in sharedpreference)
 
   fetchProducts() async {
-    Uri url = Uri.parse("https://fakestoreapi.com/products/${pid.value}");
-    var response = await https.get(url);
-    if (response.statusCode == 200) {
-      var body = response.body.toString();
-      var json = jsonDecode(body);
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    var productData = prefs.getString('productData');
 
-      log(body.toString());
-      product.value = Product.fromJson(json);
+    if (productData != null) {
+
+      log("Retrieved from SharedPreferences: $productData");
+
+      List<dynamic> jsonList = jsonDecode(productData);
+      List<Product> allProducts = jsonList.map((e) => Product.fromJson(e)).toList();
+
+      // Find the product with the matching ID
+      product.value = allProducts.firstWhere((p) => p.id == int.parse(pid.value));
     }
   }
 }
--- self_practice/lib/Utility/MyHomePage/Controller.dart
+++ self_practice/lib/Utility/MyHomePage/Controller.dart
@@ -4,6 +4,7 @@ import 'dart:developer';
 import 'package:get/get.dart';
 import 'package:http/http.dart'as https;
 import 'package:self_practice/Model/Product.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomeController extends GetxController{
 
@@ -16,18 +17,43 @@ class HomeController extends GetxController{
     super.onInit();
     getData();
   }
+   //Use Api
+  // getData()async{
+  //   Uri url = Uri.parse("https://fakestoreapi.com/products");
+  //   var response = await https.get(url);
+  //   if(response.statusCode == 200)
+  //     {
+  //       var body = response.body.toString();
+  //       var json = jsonDecode(body);
+  //
+  //       log(body.toString());
+  //       allData.value = json.map<Product>((obj) => Product.fromJson(obj)).toList();
+  //       update();
+  //     }
+  // }
 
-  getData()async{
+
+  // Use SharedPreference (Api Strore data in sharedpreference)
+  getData() async {
     Uri url = Uri.parse("https://fakestoreapi.com/products");
     var response = await https.get(url);
-    if(response.statusCode == 200)
-      {
-        var body = response.body.toString();
-        var json = jsonDecode(body);
+    if (response.statusCode == 200) {
+      var body = response.body.toString();
+      var json = jsonDecode(body);
 
-        log(body.toString());
-        allData.value = json.map<Product>((obj) => Product.fromJson(obj)).toList();
-        update();
-      }
+      log(body.toString());
+      allData.value = json.map<Product>((obj) => Product.fromJson(obj)).toList();
+      // Convert data to JSON and log it before storing
+      var productData = jsonEncode(allData.map((e) => e.toJson()).toList());
+      log("Storing in SharedPreferences: $productData");
+
+      // Store data in SharedPreferences
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      await prefs.setString('productData', jsonEncode(allData.map((e) => e.toJson()).toList()));
+
+      update();
+    }
   }
+
+
 }
