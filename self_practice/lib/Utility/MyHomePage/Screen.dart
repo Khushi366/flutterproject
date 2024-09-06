862a70f751b37074ca598fe6fe8d6268d85a9546
Khushi366 <khushiidesai2@gmail.com>
2024-09-06 19:00:25 +0530

    06-09-2024

--- self_practice/lib/Utility/MyHomePage/Screen.dart
+++ self_practice/lib/Utility/MyHomePage/Screen.dart
@@ -1,3 +1,111 @@
+
+// use Gridview
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+// import 'package:self_practice/ProductPage/Screen.dart';
+// import 'package:self_practice/Theme/AppColor.dart';
+// import 'package:self_practice/Utility/MyDrawer.dart';
+// import 'package:self_practice/View/Auth/Login/Screen.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
+// import 'Controller.dart'; // Import your HomeController
+//
+// class HomeScreen extends StatelessWidget {
+//   final  controller = Get.put(HomeController());
+//   @override
+//   Widget build(BuildContext context) {
+//
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Products",style: TextStyle(color: kWhite),),
+//         backgroundColor: kAppBarColor,
+//         centerTitle: true,
+//         actions: [
+//           IconButton(
+//               onPressed: ()async{
+//                 SharedPreferences prefs = await SharedPreferences.getInstance();
+//                  prefs.clear();
+//                 Get.offAll(() => LoginScreen());
+//               }, icon: Icon(Icons.logout))
+//         ],
+//       ),
+//         drawer: MyDrawer(),
+//         body: Obx(()=>
+//           controller.allData.isEmpty
+//           ?Center(child: CircularProgressIndicator()) // Show loading spinner
+//          : GridView.builder(
+//             gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+//               crossAxisCount: 2, // Number of columns in the grid
+//               crossAxisSpacing: 8.0, // Horizontal spacing between grid items
+//               mainAxisSpacing: 8.0, // Vertical spacing between grid items
+//               childAspectRatio: 0.75, // Aspect ratio of each grid item
+//             ),
+//             itemCount: controller.allData.length,
+//             itemBuilder: (context, index) {
+//               return GestureDetector(
+//                 onTap: (){
+//                   Get.to(
+//                         () => ProductDetailsScreen(),
+//                     arguments: controller.allData![index].id.toString(),
+//
+//                   );
+//                 },
+//                 child: _buildProductCard(
+//                   controller.allData[index].image.toString(),
+//                   controller.allData[index].title.toString(),
+//                   controller.allData[index].price.toString(),
+//                 ),
+//               );
+//             },
+//            )
+//       )
+//     );
+//   }
+//   Widget _buildProductCard(String image, String title, String price) {
+//     return Container(
+//       margin: EdgeInsets.all(2.0),
+//       padding: EdgeInsets.all(2.0),
+//       decoration: BoxDecoration(
+//         border: Border.all(color: kAppBarColor),
+//         borderRadius: BorderRadius.circular(10.0),
+//       ),
+//       child: Column(
+//         crossAxisAlignment: CrossAxisAlignment.start,
+//         children: [
+//           Expanded(
+//             child: Image.network(
+//               image,
+//               fit: BoxFit.cover,
+//             ),
+//           ),
+//           Padding(
+//             padding: const EdgeInsets.all(8.0),
+//             child: Column(
+//               crossAxisAlignment: CrossAxisAlignment.start,
+//               children: [
+//                 Text(
+//                   title,
+//                   style: TextStyle(fontWeight: FontWeight.bold),
+//                   overflow: TextOverflow.ellipsis,
+//                   maxLines: 2,
+//                 ),
+//                 SizedBox(height: 4.0),
+//                 Text(
+//                   '\$${price}',
+//                   style: TextStyle(color: Colors.green),
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+//
+
+
+
+//use Listview
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:self_practice/ProductPage/Screen.dart';
@@ -8,83 +116,79 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'Controller.dart'; // Import your HomeController
 
 class HomeScreen extends StatelessWidget {
-  final  controller = Get.put(HomeController());
+  final controller = Get.put(HomeController());
+
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-        title: Text("Products",style: TextStyle(color: kWhite),),
+        title: Text(
+          "Products",
+          style: TextStyle(color: kWhite),
+        ),
         backgroundColor: kAppBarColor,
         centerTitle: true,
         actions: [
           IconButton(
-              onPressed: ()async{
+              onPressed: () async {
                 SharedPreferences prefs = await SharedPreferences.getInstance();
-                 prefs.clear();
+                prefs.clear();
                 Get.offAll(() => LoginScreen());
-              }, icon: Icon(Icons.logout))
+              },
+              icon: Icon(Icons.logout))
         ],
       ),
-        drawer: MyDrawer(),
-        body: Obx(()=>
-          controller.allData.isEmpty
-          ?Center(child: CircularProgressIndicator()) // Show loading spinner
-         : GridView.builder(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2, // Number of columns in the grid
-              crossAxisSpacing: 8.0, // Horizontal spacing between grid items
-              mainAxisSpacing: 8.0, // Vertical spacing between grid items
-              childAspectRatio: 0.75, // Aspect ratio of each grid item
-            ),
-            itemCount: controller.allData.length,
-            itemBuilder: (context, index) {
-              return GestureDetector(
-                onTap: (){
-                  Get.to(
-                        () => ProductDetailsScreen(),
-                    arguments: controller.allData![index].id.toString(),
-
-                  );
-                },
-                child: _buildProductCard(
-                  controller.allData[index].image.toString(),
-                  controller.allData[index].title.toString(),
-                  controller.allData[index].price.toString(),
-                ),
-              );
-            },
-           )
-      )
+      drawer: MyDrawer(),
+      body: Obx(
+            () => controller.allData.isEmpty
+            ? Center(child: CircularProgressIndicator()) // Show loading spinner
+            : ListView.builder(
+          itemCount: controller.allData.length,
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              onTap: () {
+                Get.to(
+                      () => ProductDetailsScreen(),
+                  arguments: controller.allData![index].id.toString(),
+                );
+              },
+              child: _buildProductCard(
+                controller.allData[index].image.toString(),
+                controller.allData[index].title.toString(),
+                controller.allData[index].price.toString(),
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
+
   Widget _buildProductCard(String image, String title, String price) {
     return Container(
-      margin: EdgeInsets.all(2.0),
-      padding: EdgeInsets.all(2.0),
+      margin: EdgeInsets.all(8.0),
+      padding: EdgeInsets.all(8.0),
       decoration: BoxDecoration(
         border: Border.all(color: kAppBarColor),
         borderRadius: BorderRadius.circular(10.0),
       ),
-      child: Column(
+      child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Expanded(
-            child: Image.network(
-              image,
-              fit: BoxFit.cover,
-            ),
+          Image.network(
+            image,
+            fit: BoxFit.cover,
+            height: 100.0,
+            width: 100.0,
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
+          SizedBox(width: 8.0),
+          Expanded(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
                   title,
                   style: TextStyle(fontWeight: FontWeight.bold),
-                  overflow: TextOverflow.ellipsis,
-                  maxLines: 2,
                 ),
                 SizedBox(height: 4.0),
                 Text(
@@ -99,4 +203,3 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
-
